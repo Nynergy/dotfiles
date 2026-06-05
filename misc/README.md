d021c1e4c24f0b81ad8e2bceb48b57e0f6c24b2e
Nynergy <bsbuchanan99@gmail.com>
2026-06-05 17:59:57 -0400

    Added example code files for rimeglass

--- misc/README.md
+++ misc/README.md
@@ -7,4 +7,5 @@ It's where all the other stuff goes.
 ## Contents
 
 - `colorschemes` :: A directory of terminal colorschemes in .Xresources format.
+- `example_code` :: A directory of example code files for previewing colorschemes.
 - `layouts` :: A directory of screen layouts in xrandr format (sort of).
---
+++ misc/example_code/example.c
@@ -0,0 +1,10 @@
+%5%#include %6%<stdio.h>
+
+%8%// %3%%HIGHLIGHT%TODO%RESET%%8%: Write all of the code
+
+%2%%BOLD%int %RESET%main(%2%%BOLD%int %RESET%argc, %2%%BOLD%char %RESET%** argv) {
+    %8%// Print hello world message
+    %RESET%printf(%6%"Hello, world!%3%\\n%6%"%RESET%);
+
+    %1%%BOLD%return %RESET%%6%@0@%RESET%;
+}
